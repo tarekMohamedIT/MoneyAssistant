1616306a68b61478e6d1336767e1ff4b2d01659a
r3tr0 <tarekmohamedit@gmail.com>
2018-09-09 16:28:18 +0200

    Added the Apache 2.0 license.

--- app/src/main/java/com/r3tr0/moneyassistant/ui/activities/WalletManageActivity.java
+++ app/src/main/java/com/r3tr0/moneyassistant/ui/activities/WalletManageActivity.java
@@ -1,9 +1,25 @@
 package com.r3tr0.moneyassistant.ui.activities;
 
+/**
+ * Copyright 2018 Tarek Mohamed
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,11 +27,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.r3tr0.moneyassistant.R;
-import com.r3tr0.moneyassistant.logic.managers.DatabaseManager;
 import com.r3tr0.moneyassistant.core.database.models.WalletsDatabaseModel;
-import com.r3tr0.moneyassistant.utils.enums.TransitionFlags;
 import com.r3tr0.moneyassistant.core.models.Wallet;
+import com.r3tr0.moneyassistant.logic.managers.DatabaseManager;
 import com.r3tr0.moneyassistant.utils.Regex;
+import com.r3tr0.moneyassistant.utils.enums.TransitionFlags;
 
 public class WalletManageActivity extends AppCompatActivity {
 
